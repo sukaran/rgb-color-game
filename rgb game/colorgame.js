9f000d2883fc9719c64352a7b747c468ba8326ff
sukaran <32487359+sukaran@users.noreply.github.com>
2018-12-20 13:46:30 +0530

    commit
    halaluya

---
+++ rgb game/colorgame.js
@@ -0,0 +1,18 @@
+var colors = [
+    "rgb(255,0,0)",
+    "rgb(255,255,0)",
+    "rgb(0,255,0)",
+    "rgb(0,0,255)",
+    "rgb(255,255,255)"
+]
+
+var squares =document.querySelectorAll(".square");
+
+var pickedColor=colors[3];
+var colorDisplay= document.getElementbyId("colorDisplay");
+colordisplay.textContent =pickedColor;
+
+for(var i=0;i<squares.length;i++)
+{
+    squares[i].style.background=colors[i];
+}
